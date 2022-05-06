9a841e88fe887ea7c78424a62b913a2b343628bb
Salman Mohammed <salmanmohammed176@gmail.com>
2022-05-06 17:44:03 +0300

    halite-13 Adds persistence layer project

--- halite-domain/src/test/java/com/smyunis/halite/domain/Tst.java
+++ halite-domain/src/test/java/com/smyunis/halite/domain/Tst.java
@@ -1,10 +1,11 @@
 package com.smyunis.halite.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Tst {
     @Test
     public void initTest(){
-        
+        Assertions.assertTrue(true);
     }
 }

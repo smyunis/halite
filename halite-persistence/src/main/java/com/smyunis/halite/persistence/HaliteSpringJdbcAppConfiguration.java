53fbc082e2e66750b56b88fcb4da83d572e2fff0
Salman Mohammed <salmanmohammed176@gmail.com>
2022-05-23 11:29:30 +0300

    Adds property source to persistence config in persistence

--- halite-persistence/src/main/java/com/smyunis/halite/persistence/HaliteSpringJdbcAppConfiguration.java
+++ halite-persistence/src/main/java/com/smyunis/halite/persistence/HaliteSpringJdbcAppConfiguration.java
@@ -1,8 +1,10 @@
 package com.smyunis.halite.persistence;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "classpath:persistence.properties")
 public class HaliteSpringJdbcAppConfiguration {
 
 }
--- halite-web/src/main/java/com/smyunis/halite/web/controllers/CatererController.java
+++ halite-web/src/main/java/com/smyunis/halite/web/controllers/CatererController.java
@@ -3,7 +3,6 @@ package com.smyunis.halite.web.controllers;
 import com.smyunis.halite.application.caterer.CatererService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
